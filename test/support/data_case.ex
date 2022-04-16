718c9509e079c967eb7097e116284bded138c7b5
Lai Boon Hui <laiboonh@gmail.com>
2022-04-16 21:30:13 +0800

    Fix credo

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -16,6 +16,8 @@ defmodule Songcho.DataCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       alias Songcho.Repo
@@ -28,8 +30,8 @@ defmodule Songcho.DataCase do
   end
 
   setup tags do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Songcho.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(Songcho.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
     :ok
   end
 
